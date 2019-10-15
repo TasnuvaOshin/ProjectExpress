99b7487f8e97596e9a0f108c91719a806b53df90
Sumayea Rahman <sumayearahman@users.noreply.github.com>
2019-10-15 10:59:59 +0600

    15 oct 3rd update

---
+++ app/src/main/java/com/sumayea/joyexpress/Home.java
@@ -0,0 +1,14 @@
+package com.sumayea.joyexpress;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Home extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.create_delivery);
+    }
+}
